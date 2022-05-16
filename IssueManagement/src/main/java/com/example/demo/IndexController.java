df514297e031e2afda9b117de5abb81e08953d48
x-KayaNo-x <kayano.pic@gmail.com>
2022-05-16 17:31:36 +0900

    ログイン・ログアウト機能実装

--- IssueManagement/src/main/java/com/example/demo/IndexController.java
+++ IssueManagement/src/main/java/com/example/demo/IndexController.java
@@ -11,4 +11,13 @@ public class IndexController {
 		return "index";
 	}
 
+	@GetMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+	
+	@GetMapping("/logout")
+	public String showLogout() {
+		return "logout";
+	}
 }
